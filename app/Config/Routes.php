bff26c271c4773123bc6e9ea8d84958214f4d3b4
Tsimbina <tsimbinamitantsoa@gmail.com>
2026-05-13 07:34:41 +0000

    [feat] login

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,6 +9,11 @@ $routes->get('login',           'UserController::loginPage');
 $routes->post('login',          'UserController::login');
 $routes->get('logout',          'UserController::logout');
 
+// Employee Login
+$routes->match(['get', 'post'], 'employee/login',           'EmployeeController::login');
+$routes->match(['get', 'post'], 'employee/loginProcess',    'EmployeeController::loginProcess');
+$routes->get('employee/logout',          'EmployeeController::logout');
+
 // Employees - Creation only
 $routes->get('employes/create',           'EmployeeController::create');
 $routes->post('employes/save',            'EmployeeController::store');
