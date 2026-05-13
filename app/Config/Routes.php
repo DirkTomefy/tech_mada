6579f3186f2c7307062d1e2bf340a1b1767a96d7
DirkTomefy <DirkTomefy@gmail.com>
2026-05-13 10:57:08 +0300

    [fix] : routes fixed

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,13 +4,6 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 
-$routes->get('employes',                   'EmployeeController::index');
-$routes->post('employes/save',            'EmployeeController::store');
-$routes->get('employes/(:num)',           'EmployeeController::show/$1');
-$routes->get('employes/edit/(:num)',      'EmployeeController::edit/$1');
-$routes->post('employes/update/(:num)',   'EmployeeController::update/$1');
-$routes->get('employes/delete/(:num)',    'EmployeeController::delete/$1');
-
 // Employees — routes admin
 $routes->get('admin/employes',                    'EmployeeController::index');
 $routes->get('admin/employes/create',             'EmployeeController::create');
